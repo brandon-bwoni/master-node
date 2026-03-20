d26e18cacd51c2574619807eaeeebf1abce96266
Brandon Bwoni <bwonibrandon@gmail.com>
2026-03-20 08:08:40 +0200

    frontend: profile page

---
+++ auth-engine/frontend/src/components/Profile.tsx
@@ -0,0 +1,14 @@
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile page</h1>
+      <div>
+        <button className="px-4 py-2 bg-red-600 text-white font-medium">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
